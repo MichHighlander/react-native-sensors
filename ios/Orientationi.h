bfd73815fe759901921afb5d162bcb97a43ba962
MichHighlander <michael@highlander.io>
2021-05-02 18:12:10 +0300

    ??

--- ios/Orientationi.h
+++ ios/Orientationi.h
@@ -4,7 +4,7 @@
 #import <CoreMotion/CoreMotion.h>
 #import <React/RCTEventEmitter.h>
 
-@interface OrientationI : RCTEventEmitter <RCTBridgeModule> {
+@interface Orientationi : RCTEventEmitter <RCTBridgeModule> {
     CMMotionManager *_motionManager;
     int logLevel;
 }
